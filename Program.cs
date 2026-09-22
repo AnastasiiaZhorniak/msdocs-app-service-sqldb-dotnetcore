61a28a9a97d82f75c41bfb8d823d8201d3dd118c
AnastasiiaZhorniak <anastasiia.zhorniak@nure.ua>
2026-09-22 13:42:04 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,42 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using DotNetCoreSqlDb.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add database context
+// --------------------
+// Azure SQL Database
+// --------------------
 if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("MyDbConnection")));
 }
 else
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+}
+
+// --------------------
+// Azure Managed Redis
+// --------------------
+var redisConnectionString =
+    builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+
+if (!string.IsNullOrWhiteSpace(redisConnectionString))
+{
+    builder.Services.AddStackExchangeRedisCache(options =>
+    {
+        options.Configuration = redisConnectionString;
+        options.InstanceName = "TodoApp:";
+    });
+}
+else
+{
+    // Fallback для локального запуску або якщо Redis не налаштований
+    builder.Services.AddDistributedMemoryCache();
 }
 
 // Add services to the container.
@@ -26,7 +51,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
